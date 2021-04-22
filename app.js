186365866d370dc29cb551e6ed2f4ec9eb89c704
Silviu Tudor Marc <49163151+AI-P-K@users.noreply.github.com>
2021-04-22 13:08:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,8 @@ cameraTrigger.onclick = function() {
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
 
-// Draw a rectangle on the camera
+// Draw a  rectangle on the camera
 var canvas = document.getElementById("camera--sensor");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "rgb(0, 0, 255)";
-ctx.fillRect(50, 40, 30, 30);
+ctx.strokeRect(50,50, 100, 100);
